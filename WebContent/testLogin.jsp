61a03fcb5259ceaa44ba6df6a358fdc897fa52ae
EricLee3 <in.lee@isecommerce.co.kr>
2017-01-26 17:13:16 +0900

    post testing and push github

--- WebContent/testLogin.jsp
+++ WebContent/testLogin.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ page import="javax.servlet.http.HttpServletRequest" %>
-<%@ page import="com.service.sampleSelect" %>
+<%@ page import="com.service.sampleLogin" %>
 <%@ page import="com.service.command.util.*" %>
 <%@ page import="java.util.*" %>
 <%@ page import="org.apache.commons.httpclient.methods.GetMethod" %>
@@ -8,16 +8,17 @@
 <%@ page import="org.apache.commons.httpclient.HttpClient" %>
 <%@ page import="org.apache.commons.httpclient.HttpException" %>
 <%
-	sampleSelect jsonDAO	= sampleSelect.getInstance();
+	sampleLogin jsonDAO	= sampleLogin.getInstance();
 
 	String command	= StringUtil.nullTo(request.getParameter("command"),"");
 
 	String resultMessage = null;
 	
 	if (command.equals("login")) {
+		request.setCharacterEncoding("utf-8");
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
-		resultMessage = jsonDAO.selectBrand();
+		resultMessage = jsonDAO.validateID();
 	} else {
 		//resultMessage.add(0, "Anyway... FAIL!!");
 	}
---
+++ src/com/service/sampleLogin.java
@@ -0,0 +1,91 @@
+package com.service;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import com.service.command.connection.DataBaseManager;
+import com.service.command.log.Logger;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONException;
+
+public class sampleLogin {
+	
+	private static sampleLogin instance = new sampleLogin();
+
+	JSONArray jsArray;
+	public static sampleLogin getInstance() {
+		return instance;
+	}
+   
+	private sampleLogin() {
+		
+	} 
+	
+//	public List<Object> selectBrand() throws IOException {
+	public String validateID() throws IOException {
+		String methodName ="com.service.sampleLogin()";
+		Logger.debug(methodName);
+		
+		Connection 			conn	= null;
+		PreparedStatement	pstmt	= null;
+		ResultSet			rs		= null;
+		StringBuffer   	sqlBuffer  	= new StringBuffer(500);	//쿼리문
+		
+		HashMap hm 			= new HashMap();
+		List	vendorList 	= new ArrayList();
+		
+		int cnt 	= 0;
+		
+		try
+		{ 
+			conn = DataBaseManager.getConnection();
+			
+			sqlBuffer.append(" SELECT  *            ");  
+			sqlBuffer.append(" FROM    CMUSER		");
+			sqlBuffer.append(" where   user_id = ? and user_pwd = ? ");
+			
+			
+			pstmt = conn.prepareStatement(sqlBuffer.toString());
+			pstmt.setString(1, "a");
+			pstmt.setString(2, "b");
+			rs = pstmt.executeQuery();
+			
+			jsArray = new JSONArray();
+				while(rs.next())
+				{
+					JSONObject jsObj = new JSONObject();
+					jsObj.put("BRAND_NM", rs.getString("BRAND_NM"));
+					jsArray.add(jsObj);
+//					hm = new HashMap();
+//					hm.put("BRAND_NM", rs.getString("BRAND_NM"));
+//					vendorList.add(hm);
+				}
+			
+		} catch(Exception e) {
+			Logger.debug("###Error###:"+ methodName +" Error :"+ e.toString());
+		} finally {
+			try
+			{
+				if( rs !=null ) try{ rs.close(); rs = null; }catch(Exception e){}finally{rs = null;}
+				if( pstmt != null ) try{ pstmt.close(); pstmt = null; }catch(Exception e){}finally{pstmt = null;}
+				
+				DataBaseManager.close(conn);
+				if( conn!= null ) try{conn.close(); conn = null; }catch(Exception e){}finally{conn = null;}
+			}
+			catch (Exception e)
+			{
+				Logger.debug("###Error###:"+ methodName +" Error :"+ e.toString());
+			}
+		}
+		
+		return jsArray.toString();
+	}
+}
